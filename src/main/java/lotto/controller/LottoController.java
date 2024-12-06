b33d5461ff460a1c3df6ee22b6250207de46f253
7i.w__k <tizmfns1218@Naver.com>
2024-12-06 13:10:20 +0900

    feat: 초기 세팅

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,21 @@
+package lotto.controller;
+
+import lotto.view.InputViewer;
+import lotto.view.OutputViewer;
+
+public class LottoController {
+
+    private final InputViewer inputViewer;
+    private final OutputViewer outputViewer;
+
+    public LottoController(InputViewer inputViewer, OutputViewer outputViewer) {
+        this.inputViewer = inputViewer;
+        this.outputViewer = outputViewer;
+    }
+
+
+
+
+
+
+}
---
+++ src/main/java/lotto/view/OutputViewer.java
@@ -0,0 +1,11 @@
+package lotto.view;
+
+public class OutputViewer {
+
+    private static final String ERROR_SIGN = "[ERROR] ";
+
+    public void printError(Exception e) {
+        System.out.println(ERROR_SIGN + e.getMessage());
+    }
+
+}
